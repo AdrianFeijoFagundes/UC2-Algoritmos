8fd0997cc8a4715ba114f0573b8157e091efb53e
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-03-08 21:56:50 -0300

    primeiro commit

---
+++ aula03/aula03.js
@@ -0,0 +1,35 @@
+// 
+/*
+Comentário em mais de uma linha
+*/ 
+// camelCase  primeira palavra minucula demais palavras com começo em maisculas
+
+//imprime algo
+console.log("Olá mundo");
+
+// DECLARAÇÃO DE VARIVEL
+// const COMSTANTE Não pode mudar o valor depois
+
+const nome = "Adrian"
+const sobrenome = "Fagundes"
+const souEstudante = true
+
+// let MUTAVEL
+let idade = 21
+
+
+// numbers
+const temperatura = 15
+const altura = 1.79
+
+// string
+const carro = "Palio"
+const comida = "Arroz"
+
+// boolean váriaveis que só assumem verdadeiro ou falso
+const verdadeiro = true
+const falso = false
+
+console.log("Olá meu nome é ", nome, sobrenome ,"e eu tenho", idade, "anos. Sou estudante?", souEstudante)
+
+console.log(typeof nome)
