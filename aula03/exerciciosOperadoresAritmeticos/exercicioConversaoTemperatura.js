ba5cbf75326ef23d63b3c4cfa7d2952d53e45b63
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-03-14 20:18:49 -0300

    Add files via upload

---
+++ aula03/exerciciosOperadoresAritmeticos/exercicioConversaoTemperatura.js
@@ -0,0 +1,7 @@
+// Exercício de Conversão de Temperatura: Declare uma variável para armazenar uma temperatura em Celsius. Em seguida, crie uma segunda variável para armazenar a temperatura convertida para Fahrenheit. Utilize a fórmula de conversão (F = C * 9/5 + 32).
+
+let celsius = 0
+
+let fahrenheit = celsius * 9/5 + 32
+
+console.log(fahrenheit)
