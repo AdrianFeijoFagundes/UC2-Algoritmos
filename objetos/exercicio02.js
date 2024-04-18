bbe752d7d22098f6e33e1be93d3b0867e1fb2091
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-04-17 21:55:13 -0300

    Add files via upload

---
+++ objetos/exercicio02.js
@@ -0,0 +1,7 @@
+const pessoa = {
+    nome: 'Adrian',
+    idade: 21,
+    'genero-musical-favorito' : 'Rock'
+}
+
+console.log(`Essa pessoa tem o nome de ${pessoa.nome} a sua idade é ${pessoa.idade}, e seu genero musical favorito é ${pessoa['genero-musical-favorito']}`)
